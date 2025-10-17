eb11ad5194b328103962fb0cded36d9496e585e1
sergij14 <sergi.jajanidze@gmail.com>
2025-10-17 19:04:01 +0400

    refactor: extracted post view functionality

--- src/app/topics/[slug]/posts/[postId]/page.tsx
+++ src/app/topics/[slug]/posts/[postId]/page.tsx
@@ -1,25 +1,16 @@
-import { db } from '@/utils/db';
+import PostView from '@/components/posts/PostView';
 import paths from '@/utils/paths';
 import Link from 'next/link';
-import { notFound } from 'next/navigation';
 
 async function PostViewPage({ params }: { params: Promise<{ postId: string, slug: string }> }) {
     const { postId, slug } = await params;
-    const post = await db.post.findFirst({
-        where: { id: postId }
-    })
-
-    if (!post) return notFound();
 
     return (
         <div>
             <Link href={paths.topicView(slug)}>
                 {'< '}Back to {slug}
             </Link>
-            <div className='mt-2'>
-                <h4 className='text-lg font-bold'>{post.title}</h4>
-                <p>{post.content}</p>
-            </div>
+            <PostView postId={postId} />
         </div>
     )
 }
---
+++ src/components/posts/PostView.tsx
@@ -0,0 +1,19 @@
+import { db } from "@/utils/db";
+import { notFound } from "next/navigation";
+
+async function PostView({ postId }: { postId: string }) {
+    const post = await db.post.findFirst({
+        where: { id: postId }
+    })
+
+    if (!post) return notFound();
+
+    return (
+        <div className='mt-2 flex flex-col gap-2'>
+            <h4 className='text-lg font-bold'>{post.title}</h4>
+            <p className="border border-gray-200 p-4">{post.content}</p>
+        </div>
+    )
+}
+
+export default PostView

54b84aa844edaaedc8ef81e4ca5b91a735b56bd2
sergij14 <sergi.jajanidze@gmail.com>
2025-10-17 18:50:33 +0400

    feat: created post view page

--- src/app/topics/[slug]/posts/[postId]/page.tsx
+++ src/app/topics/[slug]/posts/[postId]/page.tsx
@@ -1,8 +1,26 @@
-import React from 'react'
+import { db } from '@/utils/db';
+import paths from '@/utils/paths';
+import Link from 'next/link';
+import { notFound } from 'next/navigation';
 
-function PostViewPage() {
+async function PostViewPage({ params }: { params: Promise<{ postId: string, slug: string }> }) {
+    const { postId, slug } = await params;
+    const post = await db.post.findFirst({
+        where: { id: postId }
+    })
+
+    if (!post) return notFound();
+
     return (
-        <div>PostViewPage</div>
+        <div>
+            <Link href={paths.topicView(slug)}>
+                {'< '}Back to {slug}
+            </Link>
+            <div className='mt-2'>
+                <h4 className='text-lg font-bold'>{post.title}</h4>
+                <p>{post.content}</p>
+            </div>
+        </div>
     )
 }
 

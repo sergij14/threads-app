b561b1ac83c69c9e9a81e127a34071128cc5cc96
sergij14 <sergi.jajanidze@gmail.com>
2025-10-17 19:48:36 +0400

    feat: caching calls for fetchCommentsByPostId

--- src/utils/db/queries/comments.ts
+++ src/utils/db/queries/comments.ts
@@ -1,21 +1,23 @@
 import type { Comment } from '@prisma/client';
+import { cache } from 'react';
 import { db } from '@/utils/db';
 
 export type CommentWithAuthor = Comment & {
     user: { name: string | null; image: string | null };
 };
 
-export const fetchCommentsByPostId = (postId: string): Promise<CommentWithAuthor[]> => {
-    return db.comment.findMany({
-        where: { postId },
-        include: {
-            user: {
-                select: {
-                    name: true,
-                    image: true,
+export const fetchCommentsByPostId = cache(
+    (postId: string): Promise<CommentWithAuthor[]> => {
+        return db.comment.findMany({
+            where: { postId },
+            include: {
+                user: {
+                    select: {
+                        name: true,
+                        image: true,
+                    },
                 },
             },
-        },
-    });
-}
-
+        });
+    }
+);

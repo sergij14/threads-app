b69ddbb1b1472c643c7ef325094f680164b61b84
sergij14 <sergi.jajanidze@gmail.com>
2025-10-17 16:30:02 +0400

    feat: displaying error messages in create topic form

--- src/components/topics/TopicCreateForm.tsx
+++ src/components/topics/TopicCreateForm.tsx
@@ -1,3 +1,4 @@
+'use client'
 import React, { useActionState } from 'react'
 import { Input, Button, Textarea, Popover, PopoverTrigger, PopoverContent } from '@/utils/ui'
 import { createTopic } from '@/actions'
@@ -17,8 +18,10 @@ function TopicCreateForm() {
                 <form action={action}>
                     <div className='flex flex-col gap-4 p-4 w-80'>
                         <h3 className='text-xl'>Create a Topic</h3>
-                        <Input name='name' label="Name" labelPlacement='outside' placeholder='Name' />
-                        <Textarea name='description' label="Description" labelPlacement='outside' placeholder='Descriptiin' />
+                        <Input name='name' label="Name" labelPlacement='outside' placeholder='Name'
+                            isInvalid={!!formstate.errors?.name} errorMessage={formstate.errors?.name?.join(', ')} />
+                        <Textarea name='description' label="Description" labelPlacement='outside' placeholder='Descriptiin'
+                            isInvalid={!!formstate.errors?.description} errorMessage={formstate.errors?.description?.join(', ')} />
                         <Button type='submit'>Create</Button>
                     </div>
                 </form>

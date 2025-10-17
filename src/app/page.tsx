763dd2c5c79ec72afba0e7d9c7585f77c190ae46
sergij14 <sergi.jajanidze@gmail.com>
2025-10-17 15:36:56 +0400

    feat: created TopicCreateForm component markup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,15 @@
 import React from 'react'
+import TopicCreateForm from '@/components/topics/TopicCreateForm'
 
 function HomePage() {
-  return <div>home</div>
+  return <div className='grid grid-cols-4 gap-4'>
+    <div className='col-span-3'>
+      <h3 className='text-xl my-2'>Top Posts</h3>
+    </div>
+    <div className='flex justify-end'>
+      <TopicCreateForm />
+    </div>
+  </div>
 }
 
 export default HomePage
---
+++ src/components/topics/TopicCreateForm.tsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import { Input, Button, Textarea, Popover, PopoverTrigger, PopoverContent } from '@/utils/ui'
+import { createTopic } from '@/actions'
+
+function TopicCreateForm() {
+    return (
+        <Popover placement='left'>
+            <PopoverTrigger>
+                <Button color='primary' variant='flat'>Create a Topic</Button>
+            </PopoverTrigger>
+
+            <PopoverContent>
+                <form action={createTopic}>
+                    <div className='flex flex-col gap-4 p-4 w-80'>
+                        <h3 className='text-xl'>Create a Topic</h3>
+                        <Input label="Name" labelPlacement='outside' placeholder='Name' />
+                        <Textarea label="Description" labelPlacement='outside' placeholder='Descriptiin' />
+                        <Button type='submit'>Create</Button>
+                    </div>
+                </form>
+            </PopoverContent>
+        </Popover>
+    )
+}
+
+export default TopicCreateForm

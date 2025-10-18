3557d9f67ae742291c68047693f4337f92798b47
sergij14 <sergi.jajanidze@gmail.com>
2025-10-18 11:25:01 +0400

    feat: added util for decoding slug url string

--- src/app/topics/[slug]/page.tsx
+++ src/app/topics/[slug]/page.tsx
@@ -1,9 +1,11 @@
 import PostCreateForm from '@/components/posts/PostCreateForm';
 import PostList from '@/components/posts/PostList';
+import { decodeString } from '@/utils/common';
 import { fetchPostsByTopicSlug } from '@/utils/db/queries/posts';
 
 export default async function TopicViewPage({ params }: { params: Promise<{ slug: string }> }) {
-    const { slug } = await params;
+    const { slug: slugParam } = await params;
+    const slug = decodeString(slugParam);
 
     return (
         <div className='grid grid-cols-4 gap-4'>
--- src/app/topics/[slug]/posts/[postId]/page.tsx
+++ src/app/topics/[slug]/posts/[postId]/page.tsx
@@ -2,12 +2,14 @@ import CommentCreateForm from '@/components/comments/CommentCreateForm';
 import CommentList from '@/components/comments/CommentList';
 import PostView from '@/components/posts/PostView';
 import PostViewLoading from '@/components/posts/PostViewLoading';
+import { decodeString } from '@/utils/common';
 import paths from '@/utils/paths';
 import Link from 'next/link';
 import { Suspense } from 'react';
 
 export default async function PostViewPage({ params }: { params: Promise<{ postId: string, slug: string }> }) {
-    const { postId, slug } = await params;
+    const { postId, slug: slugParam } = await params;
+    const slug = decodeString(slugParam);
 
     return (
         <div>
